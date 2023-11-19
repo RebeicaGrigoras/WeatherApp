ccc839dba8ee145350355501436d193faa3d53ca
FlorinaRebeicaGrigoras <rebeicagrigoras25@gmail.com>
2023-11-19 13:44:12 +0000

    API integration

--- script.js
+++ script.js
@@ -35,6 +35,48 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 searchCity("Dubai");
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+
+}
+
+function getForecast(city) {
+  let apiKey = "19efot2d468e33ba0114f3ae9b731180";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+  console.log(apiUrl);
+}
+
+function displayForecast(response) {
+let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+    <div class="row align-items-start">
+      <div class="col">${formatDay(day.time)}</div>
+      <div class="col" id="icon"><img src="${day.condition.icon_url}"/></div>
+      <div class="col" id="temp-max">
+        <strong>${Math.round(day.temperature.maximum)}°C</strong>
+      </div>
+      <div class="col" id="temp-min">
+        ${Math.round(day.temperature.minimum)}°C
+      </div>
+    </div>`;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+getForecast("Dubai");
+
 // API integration
 
 function refreshWeather(response) {
@@ -59,5 +101,3 @@ function searchCity(city) {
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
-
-// Weather Data
